65acefa64b1f86d1e9401230f6eff88457de94f3
Christopher Edison Santos <grenadespamr@protonmail.com>
2020-07-13 22:54:42 +0800

    Create basic_commands.py

---
+++ cogs/basic_commands.py
@@ -0,0 +1,49 @@
+# Haqua's Basic Functions
+# written by GrenadeSpamr
+
+import discord
+from discord.ext import commands
+import random
+import wikipedia
+import textwrap
+
+class BasicCommands(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
+
+    @commands.command()
+    async def haqua(self, ctx):
+        await ctx.send("Hi, I am a Heuristic Assistant offering Quality User experience or just an Assistant. Haqua!")
+
+    @commands.command(aliases=['yn', 'yesorno'])
+    @commands.is_owner()
+    async def yesno(self, ctx):
+        binary = random.randint(0, 1)
+        if binary == 0:
+            print(f'Random output: {binary}')
+            await ctx.send('Yes')
+        else:
+            print(f'Random output: {binary}')
+            await ctx.send('No')
+
+    @commands.command(aliases=['mcs', 'conchshell'])
+    async def magicconchshell(self, ctx):
+        answers = [
+            "Maybe someday.", "Nothing.", "Neither.",
+            "I don't think so.", "No.", "Yes.",
+            "Try asking again."
+            ]
+        await ctx.send(random.choice(answers), tts=False)
+
+    @commands.command()
+    async def wiki(self, ctx, *, arg):
+        wiki = wikipedia.page(arg)
+        wikiSum = textwrap.shorten(wiki.summary, 500)
+        embed = discord.Embed(title=wiki.title, url=wiki.url, description=wikiSum)
+        embed.set_thumbnail(url=wiki.images[0])
+        embed.set_footer(text='wikipedia.org')
+        await ctx.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(BasicCommands(bot))
