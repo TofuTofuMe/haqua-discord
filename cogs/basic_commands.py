4d84c71c1c0e1fdf4607f1b6f65446a142b3243e
Christopher Edison Santos <grenadespamr@protonmail.com>
2020-09-28 10:44:57 +0800

    Update basic_commands.py
    Rewritten major parts of the "wiki" command, might move it into its own cog.
    - Added redirects and an error message for non-existent articles for Wikipedia.
    - Added commands "announce" and "delete", requiring the appropriate permissions.

--- cogs/basic_commands.py
+++ cogs/basic_commands.py
@@ -37,14 +37,40 @@ class BasicCommands(commands.Cog):
         await ctx.send(random.choice(answers), tts=False)
 
     @commands.command()
-    async def wiki(self, ctx, *, arg):
+    async def wiki(self, ctx, *, article):
         wiki = wikipediaapi.Wikipedia('en')
-        wiki_page = wiki.page(arg)
-        wikiSum = textwrap.shorten(wiki_page.summary, 500)
-        embed = discord.Embed(title=wiki_page.title, url=wiki_page.canonicalurl, description=wikiSum)
-#        embed.set_thumbnail(url=wiki.images[0])
-        embed.set_footer(text='wikipedia.org')
-        await ctx.send(embed=embed)
+        try:
+            wiki_page = wiki.page(article)
+            wikiTitle = wiki_page.title
+            wikiSum = textwrap.shorten(wiki_page.summary, 500)
+            print(article != wiki_page.title)
+            if article != wiki_page.title:
+                wikiTitle = '[Redirected] ' + wiki_page.title
+                wikiSum = '**Nearest possible result given as the requested article was not available**.\n' + wikiSum
+                print("Requested page is not available.")
+            embed = discord.Embed(title=wikiTitle, url=wiki_page.fullurl, description=wikiSum)
+#           embed.set_thumbnail(url=wiki.images[0])
+            embed.set_footer(text='Wikipedia, the free encyclopedia')
+            await ctx.send(embed=embed)
+        except Exception as error:
+            await ctx.send("The requested article was unavailable. Clarify your query.")
+
+    @commands.command()
+    @commands.is_owner()
+    @commands.has_permissions(manage_messages=True)
+    async def announce(self, ctx, *, message):
+        await ctx.message.delete()
+        await ctx.send(f"{message}")
+
+    @commands.command()
+#   @commands.is_owner()
+    @commands.has_permissions(manage_messages=True)
+    async def delete(self, ctx, message_id, *, reason):
+        message_id = int(message_id)
+        message = await ctx.channel.fetch_message(message_id)
+        await message.delete()
+        await ctx.message.delete()
+        await ctx.send(f"Deleted message {reason}")
 
 def setup(bot):
     bot.add_cog(BasicCommands(bot))
