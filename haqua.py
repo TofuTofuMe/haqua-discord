d0b6881e96793cc2a3a6487f963fd5384d3d3836
Christopher Edison Santos <grenadespamr@protonmail.com>
2021-02-26 13:45:38 +0800

    Update haqua.py
    - Switched to a JSON config file for the cogs loaded on startup
    - Added a check if Haqua's nickname is changed
    - Moved 'announce' to basic commands

--- haqua.py
+++ haqua.py
@@ -2,6 +2,7 @@
 # written by GrenadeSpamr
 
 import discord
+import json
 import logging
 import signal
 from discord.ext import commands
@@ -22,8 +23,8 @@ if path.isfile(log_file):
 load_dotenv()
 discord_token = environ.get('discord_token')
 game = discord.Game("with GrenadeSpamr")
-owner_id = ' '
-bot_id = ' '
+owner_id = environ.get('owner_id')
+bot_id = environ.get('bot_id')
 
 intents = discord.Intents()
 intents.messages = True
@@ -36,16 +37,29 @@ async def on_ready():
     print(f'Logged on as {bot.user.name}')
     await bot.change_presence(status=discord.Status.online, activity=game)
     await bot.wait_until_ready()
+    await bot.application_info()
 
 @bot.event
 async def on_message(message):
-    print('[{0.created_at}] {0.guild}/{0.channel} - {0.author}: {0.content}'.format(message))
+    format = '[{0.created_at} {0.guild}/{0.channel} - [{0.id}] {0.author}: {0.content}'.format(message)
+    print(format)
+
     if bot.user in message.mentions:
-        if message.author.id == int(bot.owner_id):
-            await message.channel.send(f"Hey")
+        if str(message.author.id) == owner_id:
+            await message.channel.send("Hey :3")
         else:
-            await message.channel.send("Please do not mention me.")
+            await message.delete()
+#           await message.channel.send("Please do not mention me.")
+#           await message.channel.send(f'{message.author.id} --- {owner_id}')
     await bot.process_commands(message)
+
+@bot.event
+async def on_member_update(before, after):
+    if str(before.id) == bot_id:
+        if after.nick != 'Haqua':
+            await after.guild.me.edit(nick='Haqua')
+#    else:
+#        print(f'User {before.nick} {after.activity}')
 
 @bot.command()
 @commands.is_owner()
@@ -91,21 +105,16 @@ async def listguilds(ctx):
 
 @bot.command()
 @commands.is_owner()
-async def announce(ctx, channel_id, *, message):
-    channel_id = int(channel_id)
-    channel = bot.get_channel(channel_id)
-    await channel.send(message)
-
-@bot.command()
-@commands.is_owner()
 async def logoff(ctx):
     await ctx.send("Log off signal received. Have a good day.")
     await bot.close()
 
 if __name__ == '__main__':
-    starting_extensions = ['cogs.basic_commands', 'cogs.weather']
+    with open('config/cogs.json') as cogs_json:
+        starting_cogs = json.load(cogs_json)['starting_cogs']
+
     print(f'Discord.py {discord.__version__}')
-    for extension in starting_extensions:
+    for extension in starting_cogs:
         try:
             bot.load_extension(extension)
             print(f"Loaded {extension}")
