98df2ed4c4d09e561014cb8963464dfb848feed7
Christopher Edison Santos <grenadespamr@protonmail.com>
2021-02-26 13:32:16 +0800

    Update rainbowsix.py
    - Minor changes to stats fields
    - API issue at initial commit was since fixed and now "current rank" is now available.

--- cogs/rainbowsix.py
+++ cogs/rainbowsix.py
@@ -25,8 +25,9 @@ class RainbowSix(commands.Cog):
             platform = 'uplay'
         
         try:
+            await self.auth.refresh_session()
             player = await self.auth.get_player(username, r6.Platforms.UPLAY)
-            # rank = await player.load_rank('ASIA')
+            rank = await player.get_rank('ASIA')
             await player.check_general()
             await player.check_level()
             await player.check_queues()
@@ -38,16 +39,23 @@ class RainbowSix(commands.Cog):
             time_played = player.time_played / 3600
             kd_overall = player.kills / player.deaths
             win_overall = (player.matches_won / player.matches_played) * 100
-            kd_ranked = player.ranked.kills / player.ranked.deaths
-            win_ranked = (player.ranked.won / player.ranked.played) * 100
+          
             kd_casual = player.casual.kills / player.casual.deaths
             win_casual = (player.casual.won / player.casual.played) * 100
             
             embed.add_field(name='Level', value=player.level, inline=True)
             embed.add_field(name='Hours Played', value=int(time_played), inline=True)
-            embed.add_field(name='Current Rank', value='API BORKED', inline=False)
+            
+            if player.ranked.played > 0:
+                embed.add_field(name='Current Rank', value=f"{rank.RANKS[rank.rank_id]} ({int(rank.mmr)})\n\xb1{rank.skill_stdev:.2f}", inline=False)
             embed.add_field(name='Overall Stats', value=f"K/D: {round(kd_overall, 2)}\nWin%: {round(win_overall, 2)}", inline=False)
-            embed.add_field(name='Ranked Stats', value=f"K/D: {round(kd_ranked, 2)}\nWin%: {round(win_ranked, 2)}", inline=False)
+            
+            if player.ranked.played == 0:
+                embed.add_field(name='Ranked Stats', value=f"No data", inline=False)
+            if player.ranked.played > 0:
+                kd_ranked = player.ranked.kills / player.ranked.deaths
+                win_ranked = (player.ranked.won / player.ranked.played) * 100
+                embed.add_field(name='Ranked Stats', value=f"K/D: {round(kd_ranked, 2)}\nWin%: {round(win_ranked, 2)}", inline=False)
             embed.add_field(name='Casual Stats', value=f"K/D: {round(kd_casual, 2)}\nWin%: {round(win_casual, 2)}", inline=False)
             # embed.add_field(name='K/D', value=round(kd_ratio, 2), inline=True)
             # embed.add_field(name='KOST', value='N/A (Soon)', inline=True)
@@ -58,11 +66,12 @@ class RainbowSix(commands.Cog):
             # embed.add_field(name='Assists', value=f'{player.kill_assists}', inline=True)
             # embed.add_field(name='Top Attacker', value=max(operators, key=timeplayed), inline=True)
             embed.set_footer(text="Tom Clancy's Rainbow Six Siege")
-            self.auth.close()
+            await self.auth.close()
             await ctx.send(embed=embed)
-        except Exception:
-            self.auth.close()
-            await ctx.send(f'> Found no player under the name [{username}]')
+        
+        except Exception as error:
+            await self.auth.close()
+            await ctx.send(f'> ```{error}```An error occured looking up [{username}]')
 
 def setup(bot):
     bot.add_cog(RainbowSix(bot))
